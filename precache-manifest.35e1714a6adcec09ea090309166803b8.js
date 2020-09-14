35bfd0b62c778daf3397dc86268d6d9c74eaa8f9
Danylo Oknemifort <ultrashelter@gmail.com>
2020-09-14 18:25:48 +0200

    Updates

---
+++ precache-manifest.35e1714a6adcec09ea090309166803b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "365e5f4055403b924b04ddbbe508eab7",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "7a587ff55bb716870fa9",
+    "url": "/cv/static/css/main.de194070.chunk.css"
+  },
+  {
+    "revision": "f7cb99488f6d23a13e3b",
+    "url": "/cv/static/js/2.0b794fef.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/cv/static/js/2.0b794fef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a587ff55bb716870fa9",
+    "url": "/cv/static/js/main.7cea6443.chunk.js"
+  },
+  {
+    "revision": "9e0e921495960402e4a4",
+    "url": "/cv/static/js/runtime-main.c17789b0.js"
+  }
+]);
