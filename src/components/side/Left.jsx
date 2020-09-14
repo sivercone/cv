7710e6adfebec601164c6b0e50ab17279d9b993c
Danylo Oknemifort <ultrashelter@gmail.com>
2020-09-14 21:25:42 +0200

    update 1.1

--- src/components/side/Left.jsx
+++ src/components/side/Left.jsx
@@ -9,7 +9,7 @@ function Left({ toggleProjectsFromApp, toggleAboutFromApp }) {
             <div className="sideleft__files" onClick={toggleProjectsFromApp}>
                <div className="sideleft__files__content">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="50px" width="50px">
-                     <path d="M0 0h512v512H0z" fill="url(#pattern)" fillOpacity="1"></path>
+                     <path d="M0 0h512v512H0z" fill="transparent" fillOpacity="1"></path>
                      <path
                         d="M41 73v304.563L88.697 151H423v-30H185.514l-16-48H41zm62.303 96L43.092 455h381.605l60.211-286H103.303z"
                         fill="#111111"
@@ -22,7 +22,7 @@ function Left({ toggleProjectsFromApp, toggleAboutFromApp }) {
             <div className="sideleft__files" onClick={toggleAboutFromApp}>
                <div className="sideleft__files__content">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="50px" width="50px">
-                     <path d="M0 0h512v512H0z" fill="url(#pattern)" fillOpacity="1"></path>
+                     <path d="M0 0h512v512H0z" fill="transparent" fillOpacity="1"></path>
                      <path
                         d="M41 73v304.563L88.697 151H423v-30H185.514l-16-48H41zm62.303 96L43.092 455h381.605l60.211-286H103.303z"
                         fill="#111111"
@@ -36,7 +36,7 @@ function Left({ toggleProjectsFromApp, toggleAboutFromApp }) {
                <div className="sideleft__files__content">
                   <a style={{ textDecoration: 'none', color: 'inherit' }} href="mailto:sivercone@gmail.com">
                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="50px" width="50px">
-                        <path d="M0 0h512v512H0z" fill="url(#pattern)" fillOpacity="1"></path>
+                        <path d="M0 0h512v512H0z" fill="transparent" fillOpacity="1"></path>
                         <path
                            d="M41 73v304.563L88.697 151H423v-30H185.514l-16-48H41zm62.303 96L43.092 455h381.605l60.211-286H103.303z"
                            fill="#111111"
