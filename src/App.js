f605ee3fd9586e79405cdcf39f56af6ca80033b7
Danylo Oknemifort <ultrashelter@gmail.com>
2020-09-14 18:08:21 +0200

    update 1.0

--- src/App.js
+++ src/App.js
@@ -1,52 +1,39 @@
 import React from 'react';
-import img_folder from './assets/img/open-folder.svg';
 
-import Popup from './Popup';
+import { Header, Left, Projects, About, Rotate } from './components/index';
 
 function App() {
-   const [isOpen, setIsOpen] = React.useState(false);
+   const [isProjects, setIsProjects] = React.useState(false);
+   const [isAbout, setIsAbout] = React.useState(false);
 
-   const togglePopup = () => {
-      setIsOpen(!isOpen);
+   const toggleProjects = () => {
+      setIsProjects(!isProjects);
+      setIsAbout(false);
+   };
+   const toggleAbout = () => {
+      setIsAbout(!isAbout);
+      setIsProjects(false);
    };
 
    return (
-      <div className="container">
-         {/* <!-- side left --> */}
-         <div className="sideleft">
-            <div className="sideleft--content">
-               <div className="sideleft--title">file</div>
-               <div className="sideleft--files" onClick={togglePopup}>
-                  <div className="sideleft--files--content">
-                     <img src={img_folder} alt="folder" />
-                     <div className="sideleft--files--name">projects</div>
-                  </div>
-               </div>
-               <div className="sideleft--files">
-                  <div className="sideleft--files--content">
-                     <img src={img_folder} alt="folder" />
-                     <div className="sideleft--files--name">bio</div>
-                  </div>
-               </div>
-               <div className="sideleft--files">
-                  <div className="sideleft--files--content">
-                     <img src={img_folder} alt="folder" />
-                     <div className="sideleft--files--name">contact</div>
-                  </div>
-               </div>
-            </div>
-         </div>
-         {/* <!-- side right --> */}
-         <div className="sideright">
-            <div className="sideright--content">
-               <div className="sideright--title">opened file</div>
-            </div>
-            {/* <!-- file --> */}
-            {isOpen ? <Popup toggle={togglePopup} /> : null}
-         </div>
+      <>
+         <Rotate />
+         <Header />
+         <div className="container">
+            <Left toggleProjectsFromApp={toggleProjects} toggleAboutFromApp={toggleAbout} />
 
-         <div className="itsNotReady">this is under construction</div>
-      </div>
+            <div className="sideright">
+               <div className="sideright__content">
+                  <div className="sideright__title">opened file</div>
+               </div>
+
+               {isProjects ? <Projects toggleFromRight={toggleProjects} /> : false}
+               {isAbout ? <About toggleFromRight={toggleAbout} /> : false}
+            </div>
+
+            <div className="itsNotReady">this is under construction</div>
+         </div>
+      </>
    );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+   return (
+      <header className="header">
+         <div>SIVERCONE</div>
+         <div>2020</div>
+      </header>
+   );
+}
+
+export default Header;
